6179eff9065c1ec3a2994d185f505f0f23cf8f9c
Mariam Garcia <mgxbreu@hotmail.com>
2025-09-28 14:59:44 -0400

    ♻️ simplifly code

--- app/src/utils.py
+++ app/src/utils.py
@@ -1,4 +1,9 @@
 import datetime
 
+CURRENCY = "RD$"
+
 def convert_month_num_to_name(month_num):
     return datetime.datetime(1, month_num, 1).strftime("%B")
+
+def format_currency(amount):
+    return f"{CURRENCY} {amount:,.2f}"
